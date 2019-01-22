0d0bf72a605b7c3dcf076a521351a516053abfa1
AoyamaNaoki <45162737+AoyamaNaoki@users.noreply.github.com>
2019-01-22 14:44:43 +0900

    Problem1_3

---
+++ Problem003/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Problem003
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            string input = Console.ReadLine();
+            int total = 0;
+            for (int i = 0; i < input.Length; i++){
+                if('1' == input[i]){
+                    total += 1;
+                }    
+            }
+            Console.Write(total);
+        }
+    }
+}
